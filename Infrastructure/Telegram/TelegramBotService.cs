ffcf952d9912e6ee15dd1bf339ffb856a785d37c
shakirf <shakirfarajullayev@gmail.com>
2025-07-09 16:36:10 +0400

    added telegramBotService

---
+++ Infrastructure/Telegram/TelegramBotService.cs
@@ -0,0 +1,33 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace Infrastructure.Telegram
+{
+    public class TelegramBotService
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public TelegramBotService(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+        public async Task SendTextAsync(long chatId, string message)
+        {
+            await _botClient.SendMessage(
+                chatId: chatId,
+                text: message,
+                parseMode: ParseMode.Html
+            );
+        }
+
+        public async Task SetWebhookAsync(string url)
+        {
+            await _botClient.DeleteWebhook();
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                await _botClient.SetWebhook(url);
+            }
+        }
+    }
+}
