eae6c00b27f68598fdd3a1dfb2999e752b66bb97
shakirf <shakirfarajullayev@gmail.com>
2025-07-09 22:15:43 +0400

    added defaultConnection and added services program.cs

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,15 +1,42 @@
 using Telegram.Bot;
-using Infrastructure.Telegram;
+using Persistence;
+using Application;
+using Infrastructure.Telegram.Service;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN") ?? throw new InvalidOperationException("BOT_TOKEN is missing");
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
 
-builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken));
-builder.Services.AddSingleton<TelegramBotService>();
-
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next.Invoke();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Unhandled error: {ex.Message}");
+        throw;
+    }
+});
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
 app.MapControllers();
 app.Run();
