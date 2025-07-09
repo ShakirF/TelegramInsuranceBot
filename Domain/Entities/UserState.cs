00eaac3a274eea91a79d3a63f17e0b6f636e640b
shakirf <shakirfarajullayev@gmail.com>
2025-07-09 22:10:53 +0400

    added entities

---
+++ Domain/Entities/UserState.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class UserState
+    {
+        public int Id { get; set; }
+        public long TelegramUserId { get; set; }
+        public string CurrentStep { get; set; } = "start";
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
