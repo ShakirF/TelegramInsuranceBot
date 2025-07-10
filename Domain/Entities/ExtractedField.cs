2de3c9479f4b14939770f642fcabb657d04865d0
shakirf <shakirfarajullayev@gmail.com>
2025-07-10 23:57:42 +0400

    added ExtractedFields

---
+++ Domain/Entities/ExtractedField.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class ExtractedField
+    {
+        public int Id { get; set; }
+
+        public int DocumentId { get; set; }
+        public Document Document { get; set; } = null!;
+
+        public string FieldName { get; set; } = null!;
+        public string FieldValue { get; set; } = null!;
+        public float Confidence { get; set; }
+    }
+}
