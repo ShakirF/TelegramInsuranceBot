46e95a9658b80ba62458e57c2a035f5b517f3872
shakirf <shakirfarajullayev@gmail.com>
2025-07-10 15:55:11 +0400

    added Document

---
+++ Domain/Entities/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public long TelegramUserId { get; set; }  
+        public string FileId { get; set; } = null!; 
+        public string FileName { get; set; } = null!;
+        public string FileType { get; set; } = null!; 
+        public string LocalPath { get; set; } = null!;
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    }
+}
