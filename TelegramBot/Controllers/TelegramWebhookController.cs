e398d2b6826330a57090dc82359664b27fe91308
shakirf <shakirfarajullayev@gmail.com>
2025-07-10 15:56:31 +0400

    changed controller

--- TelegramBot/Controllers/TelegramWebhookController.cs
+++ TelegramBot/Controllers/TelegramWebhookController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Telegram.Commands;
+﻿using Application.Interfaces;
+using Application.Telegram.Commands;
 using Infrastructure.Telegram.Interface;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -11,48 +12,17 @@ namespace TelegramBot.Controllers
     [Route("api/bot/update")]
     public class TelegramWebhookController : ControllerBase
     {
-        private readonly ITelegramBotService _botService;
-        private readonly IMediator _mediator;
+        private readonly IUpdateDispatcher _dispatcher;
 
-        public TelegramWebhookController(ITelegramBotService botService, IMediator mediator)
+        public TelegramWebhookController( IUpdateDispatcher dispatcher)
         {
-            _botService = botService;
-            _mediator = mediator;
+            _dispatcher = dispatcher;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Update update)
         {
-            if (update == null || update.Type != UpdateType.Message)
-                return Ok();
-
-            var message = update.Message;
-            if (message == null || message.Type != MessageType.Text || string.IsNullOrWhiteSpace(message.Text))
-                return Ok();
-
-            if (message.Text.Trim().ToLower() == "/start")
-            {
-                var createUser = new CreateUserCommand
-                {
-                    TelegramUserId = message.Chat.Id,
-                    FirstName = message.From?.FirstName,
-                    LastName = message.From?.LastName,
-                    Username = message.From?.Username,
-                };
-                await _mediator.Send(createUser);
-
-                var startCommand = new StartCommand
-                {
-                    ChatId = message.Chat.Id,
-                    FirstName = message.From?.FirstName
-                };
-                await _mediator.Send(startCommand);
-            }
-            else
-            {
-                await _botService.SendTextAsync(message.Chat.Id, "Unknown command. Please type /start to begin.");
-            }
-
+            await _dispatcher.HandleAsync(update);
             return Ok();
         }
     }
