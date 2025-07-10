11409cf2f131173881f402532a459adfc2d2db6e
shakirf <shakirfarajullayev@gmail.com>
2025-07-10 23:55:57 +0400

    added commands

---
+++ Application/Telegram/Commands/ParseOcrCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Telegram.Commands
+{
+    public class ParseOcrCommand : IRequest<Unit>
+    {
+        public int DocumentId { get; set; }
+        public string OcrJson { get; set; } = null!;
+    }
+}
---
+++ Application/Telegram/Commands/SendExtractedFieldsCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Telegram.Commands
+{
+
+    public class SendExtractedFieldsCommand : IRequest<Unit>
+    {
+        public long ChatId { get; set; }
+        public int DocumentId { get; set; }
+    }
+}
