b6b7d69572d0bb30a16f7a1c04c6c225e65af116
shakirf <shakirfarajullayev@gmail.com>
2025-07-13 18:36:57 +0400

    added cancelPolicy command and handler

---
+++ Application/Telegram/Commands/CancelPolicyCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Telegram.Commands
+{
+    public class CancelPolicyCommand : IRequest<Unit>
+    {
+        public long ChatId { get; set; }
+    }
+}
---
+++ Application/Telegram/Handlers/CancelPolicyCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Application.Telegram.Commands;
+using Domain.Enums;
+using Infrastructure.Telegram.Interface;
+using MediatR;
+
+namespace Application.Telegram.Handlers
+{
+    public class CancelPolicyCommandHandler : IRequestHandler<CancelPolicyCommand, Unit>
+    {
+        private readonly IUserStateService _stateService;
+        private readonly ITelegramBotService _bot;
+        private readonly IMessageProvider _messageProvider;
+
+        public CancelPolicyCommandHandler(IUserStateService stateService, ITelegramBotService bot, IMessageProvider messageProvider)
+        {
+            _stateService = stateService;
+            _bot = bot;
+            _messageProvider = messageProvider;
+        }
+
+        public async Task<Unit> Handle(CancelPolicyCommand request, CancellationToken cancellationToken)
+        {
+            await _stateService.SetStepAsync(request.ChatId, UserStep.Start);
+            await _bot.SendTextAsync(request.ChatId, _messageProvider.GetPolicyCancelMessage());
+            return Unit.Value;
+        }
+    }
+}
