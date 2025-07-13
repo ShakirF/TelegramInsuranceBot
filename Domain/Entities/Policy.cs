4bf4a1138383e70f1e0ec6fc7fc884d3a27a0320
shakirf <shakirfarajullayev@gmail.com>
2025-07-14 00:56:49 +0400

    added Policy entity

---
+++ Domain/Entities/Policy.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FilePath { get; set; } = default!;
+        public string DocumentSummary { get; set; } = null!;
+        public string GptMessage { get; set; } = null!;
+        public decimal Price { get; set; }
+        public DateTime IssuedAt { get; set; }
+        public DateTime ExpiryAt { get; set; }
+        public bool IsSentToUser { get; set; } = false;
+
+        public User User { get; set; } = default!;
+    }
+}
