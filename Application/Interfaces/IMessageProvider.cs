31e6d095a26f6fb603ed927bb33478682e5ecdd4
shakirf <shakirfarajullayev@gmail.com>
2025-07-13 18:35:57 +0400

    added messageProvider

---
+++ Application/Interfaces/IMessageProvider.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Enums;
+
+namespace Application.Interfaces
+{
+    public interface IMessageProvider
+    {
+        string GetStartMessage(string? firstName);
+        string GetDocumentSavedMessage(string fileName);
+        string GetUnexpectedDocumentMessage();
+        string GetOcrDoneMessage();
+        string GetNoExtractedFieldsMessage();
+        string GetStepMismatchMessage(UserStep step);
+        string GetConfirmationPrompt();
+        string GetConfirmationThankYouMessage();
+        string GetUnknownCommandMessage();
+        string GetPassportPromptMessage();
+        string GetCarDocPromptMessage(string fileName);
+        string GetOcrErrorMessage();
+        string GetUnexpectedErrorMessage();
+        string GetNoFieldsExtractedMessage();
+        string GetPriceQuoteMessage(string price);
+        string GetPolicyConfirmedMessage();
+        string GetPolicyCancelMessage();
+        string GetPolicyFixPriceMessage();
+    }
+}
