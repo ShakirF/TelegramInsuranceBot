28eb8bfac09bb96a546628142409e90e402541df
shakirf <shakirfarajullayev@gmail.com>
2025-07-14 19:10:46 +0400

    added adminService

---
+++ Application/Interfaces/IAdminService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces
+{
+    public interface IAdminService
+    {
+        bool IsAdmin(long telegramUserId);
+    }
+}
---
+++ Infrastructure/Telegram/Service/AdminService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Telegram.Service
+{
+    public class AdminService : IAdminService
+    {
+        private readonly HashSet<long> _adminIds;
+
+        public AdminService(IConfiguration config)
+        {
+            var section = config.GetSection("AdminSettings:TelegramIds");
+            _adminIds = section.Get<long[]>()?.ToHashSet() ?? new();
+        }
+
+        public bool IsAdmin(long telegramUserId) => _adminIds.Contains(telegramUserId);
+    }
+}
