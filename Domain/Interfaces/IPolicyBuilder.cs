aca40c4c00e5c507a5babcf4ecc5dfc1322c4cd1
shakirf <shakirfarajullayev@gmail.com>
2025-07-14 19:01:32 +0400

    added policyBuilder

---
+++ Domain/Interfaces/IPolicyBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IPolicyBuilder
+    {
+        IPolicyBuilder WithUser(User user);
+        IPolicyBuilder WithSummary(string extractedData);
+        IPolicyBuilder WithGptText(string gptText);
+        IPolicyBuilder WithPrice(decimal price);
+        Task<byte[]> BuildPdfAsync();
+    }
+}
---
+++ Infrastructure/Policy/PdfPolicyBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Policy
+{
+    public class PdfPolicyBuilder : IPolicyBuilder
+    {
+        private readonly IPdfGenerator _pdfGenerator;
+        private User _user = null!;
+        private string _summary = "";
+        private string _gptText = "";
+        private decimal _price;
+
+        public PdfPolicyBuilder(IPdfGenerator pdfGenerator)
+        {
+            _pdfGenerator = pdfGenerator;
+        }
+
+        public IPolicyBuilder WithUser(User user) { _user = user; return this; }
+        public IPolicyBuilder WithSummary(string extracted) { _summary = extracted; return this; }
+        public IPolicyBuilder WithGptText(string text) { _gptText = text; return this; }
+        public IPolicyBuilder WithPrice(decimal price) { _price = price; return this; }
+
+        public Task<byte[]> BuildPdfAsync() =>
+            _pdfGenerator.GeneratePolicyPdf(_user, _summary, _gptText, _price);
+    }
+
+}
