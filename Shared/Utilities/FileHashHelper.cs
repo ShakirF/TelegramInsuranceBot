178cc7ba1a768a37eb643377ad9014ec47a2b956
shakirf <shakirfarajullayev@gmail.com>
2025-07-15 16:29:17 +0400

    added helper

---
+++ Shared/Utilities/FileHashHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace Shared.Utilities
+{
+    public static class FileHashHelper
+    {
+        public static string ComputeSHA256(Stream stream)
+        {
+            using var sha = SHA256.Create();
+            stream.Position = 0;
+            var hash = sha.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
