96d2c7c815056e4b6981bc038f888f9ded1ccbb8
shakirf <shakirfarajullayev@gmail.com>
2025-07-15 16:28:53 +0400

    added dbSet

--- Persistence/DbContext/AppDbContext.cs
+++ Persistence/DbContext/AppDbContext.cs
@@ -11,5 +11,10 @@ namespace Persistence.DbContext
         public DbSet<UserState> UserStates => Set<UserState>();
         public DbSet<Document> Documents => Set<Document>();
         public DbSet<ExtractedField> ExtractedFields => Set<ExtractedField>();
+        public DbSet<Policy> Policies => Set<Policy>();
+        public DbSet<Conversation> Conversations => Set<Conversation>();
+        public DbSet<Error> Errors => Set<Error>();
+        public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+        public DbSet<PolicyEvent> PolicyEvents => Set<PolicyEvent>();
     }
 }
