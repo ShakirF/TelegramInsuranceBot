b1a0dc766f26b2616f2403c733f3299c6ecc2863
shakirf <shakirfarajullayev@gmail.com>
2025-07-15 16:29:10 +0400

    added contants

---
+++ Shared/Constants/PolicyConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Constants
+{
+    public static class PolicyConstants
+    {
+        public const decimal DefaultPolicyPrice = 100m;
+        public const int PolicyValidityDays = 7;
+        public const string PolicyFilePrefix = "policy_";
+    }
+}
